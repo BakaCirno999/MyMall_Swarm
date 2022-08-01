273c2566f5057328d445ce663f8db5d45fc198fe
BakaCirno999 <2223273754@qq.com>
2022-08-01 08:06:45 +0800

    create

---
+++ mall-admin/src/main/java/com/mymall/admin/dao/CmsPrefrenceAreaProductRelationDao.java
@@ -0,0 +1,17 @@
+package com.mymall.admin.dao;
+
+import com.mymall.mbg.model.CmsPrefrenceAreaProductRelation;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 自定义优选和商品关系操作Dao
+ *
+ */
+public interface CmsPrefrenceAreaProductRelationDao {
+    /**
+     * 批量创建
+     */
+    int insertList(@Param("list") List<CmsPrefrenceAreaProductRelation> prefrenceAreaProductRelationList);
+}
